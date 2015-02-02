dc86af84b1bda605f9440d6dbadb84a6ba6354aa
tonypai <tony77794@gmail.com>
2015-02-02 19:40:39 +0800

    移除skybox以及charactercontroller套件、增加移動平台關卡

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	Animator anim;
 	public bool isJumping = false;
+	public bool onPlatform = false;
 	public float speed = 3f;
 	public float jumpForce = 800f;
 
@@ -18,22 +19,40 @@ public class PlayerMovement : MonoBehaviour {
 		if(isJumping == false && Input.GetButtonDown("Jump")) {
 			isJumping = true;
 			anim.SetBool("IsWalking", false);
-			rigidbody.AddForce(new Vector3(0, jumpForce, 0));
-		}
 
-		rigidbody.velocity = new Vector3(speed, rigidbody.velocity.y, 0);
+			/*float forwardForce;
+			if(onPlatform == true)forwardForce = 400;
+			else forwardForce = 0;*/
+
+			rigidbody.AddForce(new Vector3((onPlatform)?400:0, jumpForce, 0));
+		}
+		else if(onPlatform == true);
+		else {
+			rigidbody.velocity = new Vector3(speed, rigidbody.velocity.y, 0);
+		}
 	}
 
 	void OnCollisionEnter (Collision coll) {
 		if(coll.collider.tag == "Ground") {
 			isJumping = false;
+			onPlatform = false;
 			anim.SetBool("IsWalking", true);
 		}
 	}
 
-	void OnCollisionStay(Collision collision) {
-		foreach (ContactPoint contact in collision.contacts) {
-			Debug.DrawRay(contact.point, contact.normal, Color.white);
+	void OnCollisionStay(Collision coll) {
+		if(coll.collider.tag == "Platform") {
+			isJumping = false;
+			onPlatform = true;
+			/*transform.position = new Vector3(
+				coll.transform.position.x, 
+				transform.position.y, 
+				transform.position.z
+			);*/
+			transform.parent = coll.transform;
+		}
+		else {
+			transform.parent = null;
 		}
 	}
 }

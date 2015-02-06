c9da58a4056c49cba3c8d45946d5ea06adbf6394
tonypai <tony77794@gmail.com>
2015-02-06 20:50:00 +0800

    實作怪物死亡、射擊火花粒子與彈道線(LineRenderer)

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour {
+
+	Animator walkAnim;
+	Animator monAnim;
+
+	void Start () {
+		walkAnim = transform.Find ("Main").GetComponent<Animator> ();
+		monAnim = transform.Find ("Main").Find ("Animation").GetComponent<Animator> ();
+	}
+	
+	void KillByPlayer () {
+		rigidbody.useGravity = false;
+		rigidbody.isKinematic = true;
+		monAnim.SetTrigger ("dead");
+		walkAnim.enabled = false;
+	}
+}
--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -2,15 +2,42 @@
 using System.Collections;
 
 public class PlayerShoot : MonoBehaviour {
+
+	public Transform gunParticles;
+	public LineRenderer gunLine;
+	Vector3 startPos;
+
 	void Update () {
-		if(Input.GetButtonDown("Fire1")) {
-			Ray ray = new Ray(transform.position + Vector3.up * .5f, Vector3.right);
+
+		if(Input.GetButtonDown ("Fire1")) {
+
+			startPos = transform.position + Vector3.right * .8f + Vector3.up * .5f + Vector3.back * .5f;
+
+			gunParticles.particleSystem.Play();
+
+			gunLine.enabled = true;
+			gunLine.SetPosition(0, startPos);
+
+			Ray ray = new Ray(startPos, Vector3.right);
 			RaycastHit hit;
-			
-			if(Physics.Raycast(ray, out hit, 4f)) {
-				Debug.Log (hit.transform.tag);
+
+			if(Physics.Raycast(ray, out hit, 6f)) {
+				if(hit.transform.tag == "Enemy") {
+					// Destroy (hit.transform.gameObject);
+					hit.transform.SendMessage("KillByPlayer");
+				}
+				gunLine.SetPosition(1, hit.point);
 			}
+			else {
+				gunLine.SetPosition(1, ray.origin + ray.direction * 6f);
+			}
+			StartCoroutine("FireVanish");
 		}
-		Debug.DrawRay(transform.position + Vector3.up * .5f, Vector3.right * 4f, Color.green);
+		Debug.DrawRay(startPos, Vector3.right * 6f, Color.green);
+	}
+
+	IEnumerator FireVanish () {
+		yield return new WaitForSeconds (.03f);
+		gunLine.enabled = false;
 	}
 }
